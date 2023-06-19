ab0b5eb32fe99ae1ce9675d33494117200cad721
shiwy15 <shiwy15@gmail.com>
2023-06-19 19:27:06 +0900

    localhost --> 3.36.77.22로 변경

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ dotenv.config({path : './.env'});
 
 // CORS 옵션 설정
 const corsOptions = {
-  origin: 'http://3.36.77.22:3000', // 클라이언트 도메인을 명시적으로 지정하면 보안 상의 이유로 해당 도메인만 요청 허용 가능
+  origin: 'http://3.36.77.22:3000', // 클라이언트 도메인을 명시적으로 지정하면 보안 상의 이유로 해당 도메인만 요청 허용 가능(3.36.77.22)
   methods: 'GET, POST',
   allowedHeaders:  [
     "Content-Type",
