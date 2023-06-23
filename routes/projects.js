50f0caf68d9fe909914e136697931726637c5f5b
HyeonTee <gusxo1820@icloud.com>
2023-06-23 19:21:31 +0900

    fix : projectDelete

--- routes/projects.js
+++ routes/projects.js
@@ -196,17 +196,8 @@ router.delete('/project/:projectId', async (req, res) => {
             return res.status(404).json({ message: 'Project not found.' });
         }
 
-        // Loop through nodeId array and delete each node
-        for (let nodeId of project.nodeIds) {
-            await Node.findByIdAndDelete(nodeId);
-        }
-
-        // Loop through edgeId array and delete each edge
-        for (let edgeId of project.edgeIds) {
-            await Edge.findByIdAndDelete(edgeId);
-        }
-
-        // Finally, delete the project
+        await Node.findByIdAndDelete(project.nodeId);
+        await Edge.findByIdAndDelete(project.edgeId);
         await Project.findByIdAndDelete(projectId);
         
         // Remove the project's id from the user's projectId array
