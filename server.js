c8f87b168fe82e1ef2fe99a93b257f5156a9ebc8
HyeonTee <gusxo1820@icloud.com>
2023-06-23 23:30:12 +0900

    feat : corsOptions 수정

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const { MongoClient } = require('mongodb');
 
 // CORS 옵션 설정
 const corsOptions = {
-  origin: ['https://phodo.store', 'http://143.248.198.210', 'http://192.249.28.27'], // 클라이언트 도메인을 명시적으로 지정하면 보안 상의 이유로 해당 도메인만 요청 허용 가능
+  origin: 'https://phodo.store, http://143.248.198.210, http://192.249.28.27', // 클라이언트 도메인을 명시적으로 지정하면 보안 상의 이유로 해당 도메인만 요청 허용 가능
   methods: 'GET, POST',
   allowedHeaders:  [
     "Content-Type",
